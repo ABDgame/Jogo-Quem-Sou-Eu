4c66be785c5613ff1403aab868cc5047f3efaa6a
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-08-17 22:02:24 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,164 @@ const listPersonagens = [
   },
   personagem002{
     nome = "ARANHA",
+    imagem = "./img/ARANHA.jpg"
+  },
+  personagem003{
+    nome = "ASTRONAUTA",
+    imagem = "./img/ASTRONAUTA.jpg"
+  },
+  personagem004{
+    nome = "BARRACA",
+    imagem = "./img/BARRACA.jpg"
+  },
+  personagem005{
+    nome = "BARRIL",
+    imagem = "./img/BARRIL.jpg"
+  },
+  personagem006{
+    nome = "BASQUETE",
+    imagem = "./img/BASQUETE.jpg"
+  },
+  personagem007{
+    nome = "BELICHE",
+    imagem = "./img/BELICHE.jpg"
+  },
+  personagem008{
+    nome = "BETERRABA",
+    imagem = "./img/BETERRABA.jpg"
+  },
+  personagem009{
+    nome = "BIBLIOTECA",
+    imagem = "./img/BIBLIOTECA.jpg"
+  },
+  personagem010{
+    nome = "BICICLETA",
+    imagem = "./img/BICICLETA.jpg"
+  },
+  personagem011{
+    nome = "BIQUINI",
+    imagem = "./img/BIQUINI.jpg"
+  },
+  personagem012{
+    nome = "BOLACHA",
+    imagem = "./img/BOLACHA.jpg"
+  },
+  personagem013{
+    nome = "BOLICHE",
+    imagem = "./img/BOLICHE.jpg"
+  },
+  personagem014{
+    nome = "BOMBEIRO",
+    imagem = "./img/BOMBEIRO.jpg"
+  },
+  personagem015{
+    nome = "BORRACHA",
+    imagem = "./img/BORRACHA.jpg"
+  },
+  personagem016{
+    nome = "BUMERANGUE",
+    imagem = "./img/BUMERANGUE.jpg"
+  },
+  personagem017{
+    nome = "BUQUE",
+    imagem = "./img/BUQUE.jpg"
+  },
+  personagem018{
+    nome = "BUQUE",
+    imagem = "./img/BUQUE.jpg"
+  },
+  personagem019{
+    nome = "BURRO",
+    imagem = "./img/BURRO.jpg"
+  },
+  personagem020{
+    nome = "BUSSULA",
+    imagem = "./img/BUSSULA.jpg"
+  },
+  personagem021{
+    nome = "CABRITO",
+    imagem = "./img/CABRITO.jpg"
+  },
+  personagem022{
+    nome = "CACHOEIRA",
+    imagem = "./img/CACHOEIRA.jpg"
+  },
+  personagem023{
+    nome = "CACHORRO",
+    imagem = "./img/CACHORRO.jpg"
+  },
+  personagem024{
+    nome = "CAMINHÃO",
+    imagem = "./img/CAMINHAO.jpg"
+  },
+  personagem025{
+    nome = "CANHÃO",
+    imagem = "./img/CANHAO.jpg"
+  },
+  personagem026{
+    nome = "CARANGUEJO",
+    imagem = "./img/CARANGUEJO.jpg"
+  },
+  personagem027{
+    nome = "CARROSSEL",
+    imagem = "./img/CARROSSEL.jpg"
+  },
+  personagem028{
+    nome = "CEGONHA",
+    imagem = "./img/CEGONHA.jpg"
+  },
+  personagem029{
+    nome = "COBRA",
+    imagem = "./img/COBRA.jpg"
+  },
+  personagem030{
+    nome = "COELHO",
+    imagem = "./img/COELHO.jpg"
+  },
+  personagem031{
+    nome = "COZINHA",
+    imagem = "./img/COZINHA.jpg"
+  },
+  personagem032{
+    nome = "DEGRAUS",
+    imagem = "./img/DEGRAUS.jpg"
+  },
+  personagem033{
+    nome = "DEPRESSA",
+    imagem = "./img/DEPRESSA.jpg"
+  },
+  personagem034{
+    nome = "DESENHO",
+    imagem = "./img/DESENHO.jpg"
+  },
+  personagem035{
+    nome = "DESTRUÍDA",
+    imagem = "./img/DESTRUIDA.jpg"
+  },
+  personagem036{
+    nome = "DEZEMBRO",
+    imagem = "./img/DEZEMBRO.jpg"
+  },
+  personagem037{
+    nome = "ASTRONAU",
     imagem = "./img/.jpg"
   },
   personagem003{
     nome = "ASTRONAUTA",
     imagem = "./img/.jpg"
   },
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ]
